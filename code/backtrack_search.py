e1e5a47fd7d81b8898db3f47141e20f45569c3f6
Carter Howell <lobstahimpostah@gmail.com>
2025-11-02 18:43:05 -0500

    backtrack_solve function implemented

--- code/backtrack_search.py
+++ code/backtrack_search.py
@@ -6,8 +6,22 @@ def next_empty(grid):
 
     return None
 
-def is_valid_move(grid, row, col):
+def is_valid_move(grid, row, col, num):
     return None
 
 def backtrack_solve(grid):
-    return None
+    empty = next_empty(grid)
+    if empty is None:
+        return True
+
+    row, col = empty
+
+    for num in range(1, len(grid)):
+        if is_valid_move(grid, row, col, num):
+            grid[row][col] = num
+            if backtrack_solve(grid):
+                return True
+
+            grid[row][col] = 0
+
+    return False
